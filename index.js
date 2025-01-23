e285f1650b10bd29223b3b8868b65eb310c9decb
unknown <julian>
2025-01-22 22:13:49 -0500

    avance 22 enero

--- index.js
+++ index.js
@@ -6,3 +6,24 @@ console.log("Hola sapo perro");
 
 conexion();
 
+const app = express();
+const puerto = 9700;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/ruta1", (req, res)=>{
+    console.log("mensaje desde ruta1");
+
+    return res.status(200).send({
+        autor:"julian",
+        edad:"38",
+        estado:"casado"
+    })
+});
+
+
+app.listen(puerto, ()=>{
+    console.log(`Servidor escuchando en el puerto ${puerto}`);
+})
